b14e29533c75307296ff80d2723372ca43dd993c
Gowthami236 <gowthu.manoj>
2017-01-09 15:35:52 +0530

    second comit

--- src/com/example/test/MainActivity.java
+++ src/com/example/test/MainActivity.java
@@ -1,35 +1,38 @@
 package com.example.test;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.activity.R;
 
 
+@SuppressWarnings("deprecation")
 public class MainActivity extends ActionBarActivity {
+	Button helBut;
+	EditText textInput2;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		helBut = (Button) findViewById(R.id.b1);
+		textInput2 = (EditText) findViewById(R.id.text_2);
+		helBut.setOnClickListener(new View.OnClickListener() {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+			@Override
+			public void onClick(View arg0) {
+				String data1 = textInput2.getText().toString();
+				Intent i = new Intent(MainActivity.this, Second_activity.class);
+				i.putExtra("username", data1);
+				startActivity(i);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+			}
+		});
+	}
+
 }
+
--- src/com/example/test/Second_activity.java
+++ src/com/example/test/Second_activity.java
@@ -1,5 +1,42 @@
+
 package com.example.test;
 
-public class Second_activity {
+
+import com.example.activity.R;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Second_activity extends Activity {
+	TextView result;
+	TextView result1;
+	TextView result2;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_sec);
+		result = (TextView) findViewById(R.id.text2);
+		result1 = (TextView) findViewById(R.id.text);
+		result2 = (TextView) findViewById(R.id.text5);
+		Typeface tfont = Typeface.createFromAsset(getAssets(),
+				"AlexBrush-Regular.ttf");
+		Typeface t1font = Typeface.createFromAsset(getAssets(),
+				"BlackOpsOne-Regular.ttf");
+		Typeface t2font = Typeface.createFromAsset(getAssets(),
+				"HomemadeApple.ttf");
+		Intent i = getIntent();
+		String datafrmMain = i.getStringExtra("username");
+		result.setTypeface(tfont);
+		result.setText("Hai " + datafrmMain);
+		result1.setTypeface(t1font);
+		result1.setText("Hello " + datafrmMain);
+		result2.setTypeface(t2font);
+		result2.setText("Welcome " + datafrmMain);
+
+	}
 
 }
